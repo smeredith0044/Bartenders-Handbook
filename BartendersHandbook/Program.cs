61c50b211a6688a3567bb728eed2d9cc9aa17ad2
smeredith0044 <sydney.meredith@ymail.com>
2023-03-27 15:13:04 -0400

    added more rows to ouput and converted array to string

--- BartendersHandbook/Program.cs
+++ BartendersHandbook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using Npgsql;
 
 class Sample
@@ -11,14 +12,14 @@ class Sample
         conn.Open();
 
         // Define a query
-        NpgsqlCommand command = new NpgsqlCommand("SELECT name, ingredients, flavor_profile FROM ”cocktails”", conn);
+        NpgsqlCommand command = new NpgsqlCommand("select id, name, array_to_string(ingredients, ',') as ingredients, flavor_profile from ”cocktails”", conn);
 
         // Execute the query and obtain a result set
         NpgsqlDataReader dr = command.ExecuteReader();
 
         // Output rows
         while (dr.Read())
-            Console.Write("{0}\t{1} \n", dr[0], dr[1]);
+            Console.Write("{0}\t{1}\t{2}\t{3} \n", dr[0], dr[1], dr[2], dr[3]); 
 
         conn.Close();
 
