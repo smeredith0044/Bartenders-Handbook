20de30c72c8876083dbc6edcac35fd2b0bddb8d7
smeredith0044 <sydney.meredith@ymail.com>
2023-03-31 10:57:57 -0400

    updated progam.cs, added a class that will allow users to add or delete cocktails

---
+++ BartendersHandbook/Cocktail.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Npgsql;
+
+public class Cocktail
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Ingredients { get; set; }
+    public string FlavorProfile { get; set; }
+
+    public static List<Cocktail> GetAllCocktails()
+    {
+        List<Cocktail> cocktails = new List<Cocktail>();
+
+        try
+        {
+            // Connect to the PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=sydney;" +
+            "Password=Elliesue2*;Database=bartenders_handbook;");
+            conn.Open();
+
+            // Define a SELECT statement to retrieve all cocktail records
+            string sql = "SELECT * FROM ”cocktails” ORDER BY name";
+
+            // Execute the SELECT statement and obtain a result set
+            NpgsqlCommand command = new NpgsqlCommand(sql, conn);
+            NpgsqlDataReader dr = command.ExecuteReader();
+
+            // Iterate over the result set, creating a Cocktail object for each row
+            while (dr.Read())
+            {
+                Cocktail cocktail = new Cocktail();
+                cocktail.Id = dr.GetInt32(0);
+                cocktail.Name = dr.GetString(1);
+                cocktail.Ingredients = new List<string>((string[])dr.GetValue(2));
+                cocktail.FlavorProfile = dr.GetString(3);
+                cocktails.Add(cocktail);
+            }
+
+            conn.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        return cocktails;
+    }
+
+    public static void AddCocktail(Cocktail cocktail)
+    {
+        try
+        {
+            // Connect to the PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=sydney;" +
+            "Password=Elliesue2*;Database=bartenders_handbook;");
+            conn.Open();
+
+            // Define an INSERT statement to add the new cocktail record
+            string sql = "INSERT INTO ”cocktails” (name, ingredients, flavor_profile) " +
+                "VALUES (@name, @ingredients, @flavor_profile)";
+
+            // Execute the INSERT statement, passing in the cocktail object's properties as parameters
+            NpgsqlCommand command = new NpgsqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@name", cocktail.Name);
+            command.Parameters.AddWithValue("@ingredients", cocktail.Ingredients.ToArray());
+            command.Parameters.AddWithValue("@flavor_profile", cocktail.FlavorProfile);
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+
+    public static void UpdateCocktail(Cocktail cocktail)
+    {
+        try
+        {
+            // Connect to the PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=sydney;" +
+            "Password=Elliesue2*;Database=bartenders_handbook;");
+            conn.Open();
+
+            // Define an UPDATE statement to modify the existing cocktail record
+            string sql = "UPDATE ”cocktails” SET name = @name, ingredients = @ingredients, " +
+                "flavor_profile = @flavor_profile WHERE id = @id";
+
+            // Execute the UPDATE statement, passing in the cocktail object's properties as parameters
+            NpgsqlCommand command = new NpgsqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@id", cocktail.Id);
+            command.Parameters.AddWithValue("@name", cocktail.Name);
+            command.Parameters.AddWithValue("@ingredients", cocktail.Ingredients.ToArray());
+            command.Parameters.AddWithValue("@flavor_profile", cocktail.FlavorProfile);
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
--- BartendersHandbook/Program.cs
+++ BartendersHandbook/Program.cs
@@ -2,26 +2,63 @@
 using System.Data;
 using Npgsql;
 
-class Sample
+class sample
 {
     static void Main(string[] args)
     {
-        // Connect to a PostgreSQL database
-        NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=sydney; " +
+        try
+        {
+            // Connect to a PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=sydney; " +
             "Password=Elliesue2*;Database=bartenders_handbook;");
-        conn.Open();
+            conn.Open();
 
-        // Define a query
-        NpgsqlCommand command = new NpgsqlCommand("select id, name, array_to_string(ingredients, ',') as ingredients, flavor_profile from ”cocktails”", conn);
+            // Prompt the user for a search term
+            Console.WriteLine("Enter a cocktail name or ingredient:");
+            string searchTerm = Console.ReadLine().ToLower();
 
-        // Execute the query and obtain a result set
-        NpgsqlDataReader dr = command.ExecuteReader();
+            // Define a parameterized query with a WHERE clause to filter results based on the search term
+            NpgsqlCommand command = new NpgsqlCommand("SELECT id, name, ingredients, flavor_profile FROM ”cocktails” WHERE LOWER(name) LIKE '%' || @searchTerm || '%' OR @searchTerm = ANY(ingredients)", conn);
+            command.Parameters.AddWithValue("@searchTerm", searchTerm);
 
-        // Output rows
-        while (dr.Read())
-            Console.Write("{0}\t{1}\t{2}\t{3} \n", dr[0], dr[1], dr[2], dr[3]); 
+            // Execute the query and obtain a result set
+            NpgsqlDataReader dr = command.ExecuteReader();
 
-        conn.Close();
+            // Check if any results were returned
+            if (dr.HasRows)
+            {
+                // Output the column headers
+                Console.WriteLine("ID\tName\tIngredients\tFlavor Profile");
 
+                // Output the search results
+                while (dr.Read())
+                {
+                    int id = dr.GetInt32(0);
+                    string name = dr.GetString(1);
+                    string[] ingredientsArr = (string[])dr.GetValue(2);
+                    string ingredients = string.Join(", ", ingredientsArr);
+                    string flavorProfile = dr.GetString(3);
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", id, name, ingredients, flavorProfile);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No results found for '{0}'", searchTerm);
+            }
+
+            conn.Close();
+        }
+        catch (Exception e)
+        {
+            //Catch errors and write to error.txt file in the debug folder 
+            string fileName = "error.txt";
+            string destPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            Console.WriteLine("Oops, an error occurred. Please see " + destPath);
+            System.IO.File.WriteAllText(destPath, e.ToString());
+        }
+
+        // Wait for user input before closing the console window
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 }
